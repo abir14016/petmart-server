1d1cfb0c006be2a35d0a145722c76daf443dc967
abir14016 <abir14016@gmail.com>
2023-03-03 20:37:23 +0600

    server connected with mongoDB database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,6 +8,23 @@ const port = process.env.PORT || 5000;
 //middlewares
 app.use(cors());
 app.use(express.json());
+
+//connect with mongoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v9eddyp.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("database connected");
+    }
+    finally {
+
+    }
+};
+
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send('Hello from petMart!');
