eaf0b08339793d3e6c72f4b2481ff880dbdc3381
abir14016 <abir14016@gmail.com>
2023-03-07 21:39:31 +0600

    user put and get api

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config();
 const app = express();
@@ -18,6 +19,7 @@ async function run() {
         await client.connect();
         const sampleCollection = client.db('petmart').collection('sampleCollection');
         const postCollection = client.db('petmart').collection('postCollection');
+        const userCollection = client.db('petmart').collection('userCollection');
 
         //load sample collection
         app.get('/sample', async (req, res) => {
@@ -33,6 +35,29 @@ async function run() {
             const cursor = postCollection.find(query);
             const posts = await cursor.toArray();
             res.send(posts);
+        });
+
+
+        //load all users
+        app.get('/user', async (req, res) => {
+            const query = {};
+            const cursor = userCollection.find(query);
+            const users = await cursor.toArray();
+            res.send(users);
+        });
+
+        //put user to database
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            const filter = { email: email };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: user,
+            };
+            const result = await userCollection.updateOne(filter, updateDoc, options);
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "10d" });
+            res.send({ result, token });
         });
     }
     finally {
